4e4707df8e77f51187c9bfb3d942a1e7c0131b45
Adriana García Rivero <a.grivero@alumnos.upm.es>
2018-03-23 13:22:17 +0100

    P4

---
+++ out.js
@@ -0,0 +1,33 @@
+/**
+ * Created by andrea.garciaf on 1/03/18.
+ */
+
+const figlet = require('figlet');
+const chalk = require('chalk');
+
+
+const colorize = (msg, color) => {
+    if(typeof color !== "undefined"){
+        msg = chalk[color].bold(msg);
+    }
+    return msg;
+};
+
+const log = (socket, msg, color) => {
+    socket.write(colorize(msg, color) + "\n");
+};
+
+const biglog = (socket, msg, color) => {
+    log(socket, figlet.textSync(msg, { horizontalLayout: 'full' }), color);
+};
+
+const errorlog = (socket, emsg) => {
+   socket.write(`${colorize("Error", "red")}: ${colorize(colorize(emsg, "red"), "bgYellowBright")}\n`);
+};
+
+exports = module.exports = {
+    colorize,
+    log,
+    biglog,
+    errorlog
+};
